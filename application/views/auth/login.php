be2765abf8d66c413346ebaa6e2a33145191f7b3
Kuswandi <wandinak17@gmail.com>
2019-07-07 13:56:41 +0700

    Delete pace login page

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -18,9 +18,6 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets') ?>/css/sb-admin-2.min.css" rel="stylesheet">
-
-    <link rel="stylesheet" href="<?= base_url('assets') ?>/css/loader1.css">
-    <script src="<?= base_url('assets') ?>/js/pace.min.js"></script>
 </head>
 
 <body class="bg-gradient-info">
