5ab310d0112ebd4f65198448012521bcd643f9e7
Shell Rean <reanyusufali@gmail.com>
2019-07-09 00:40:30 +0700

    Dashboard user thanks

---
+++ application/views/dashboard/index.php
@@ -0,0 +1,10 @@
+<div class="container-fluid">
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+          For my teachers
+        </div>
+        <div class="card-body">
+            Thank you for being so patient, and helping me improve! Teachers like you are hard to find, and I’m eternally grateful for everything you’ve taught me.
+        </div>
+    </div>
+</div> 
