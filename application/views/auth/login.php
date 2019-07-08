4288b365f0585ab1a51d9d9897f8f2f6fa031133
Shell Rean <reanyusufali@gmail.com>
2019-07-09 00:31:02 +0700

    Login delete pace

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -18,13 +18,6 @@
 
     <!-- Custom styles for this template-->
     <link href="<?= base_url('assets') ?>/css/sb-admin-2.min.css" rel="stylesheet">
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> Delete pace login page
-=======
->>>>>>> Fix view login
 </head>
 
 <body class="bg-gradient-info">
